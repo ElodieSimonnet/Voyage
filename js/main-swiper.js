b32fbe6860bf2baac88ee8ece625f643df2abae2
Julien <Julien.goisset@gmail.com>
2025-04-15 20:06:45 +0200

    Changed the pagination, hidding it when on last slide.

--- js/main-swiper.js
+++ js/main-swiper.js
@@ -10,4 +10,22 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
         clickable: true,
     },
+    // On défini une action sur l'événement 'slideChange'
+    on: {
+        slideChange: function () {
+            // On créer un flag (true/false) en vérifiant si la slide actuelle est la dernière en comparant à la taille de la liste des slides (attention la liste commence à 0 et non 1).
+            const isLastSlide = this.activeIndex === this.slides.length - 1;
+            // On créer un objet JS permettant d'interagir avec la pagination dans le DOM.
+            const paginationEl = document.querySelector('.swiper-pagination');
+
+            // On vérifie si on est sur la dernière slide
+            if (isLastSlide) {
+                // Si oui, on applique le style CSS pour masquer notre pagination.
+                paginationEl.style.display = 'none';
+            } else {
+                // Sinon on affiche la pagination.
+                paginationEl.style.display = 'block';
+            }
+        }
+    }
 });
